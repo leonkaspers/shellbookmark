31057c2d97d8a475dd68662c298a16058c755226
Leon Kaspers <117840392+leonkaspers@users.noreply.github.com>
2022-11-23 08:49:18 +0100

    Updated Help Text

--- shellbookmark5/Help.cs
+++ shellbookmark5/Help.cs
@@ -23,9 +23,14 @@ namespace shellbookmark5
         {
             Console.WriteLine("You can use _shellbookmark_ from every folder using cmd. Just type shellbookmark followed by one of the following commands:\n");
             Console.WriteLine("- save [bookmark name]: saves current working directory under the name of [bookmark name]");
+            Console.WriteLine("- go (2 options)");
             Console.WriteLine("- go [bookmark name]: changes the working directory to the with [bookmark name] associated folder");
+            Console.WriteLine("\t- go [bookmark name]: changes the working directory to the with [bookmark name] associated folder");
+            Console.WriteLine("\t- go [bookmark index]: changes the working directory to the with [bookmark index] associated folder (you can look up the index using the list command)");
             Console.WriteLine("- list: lists all saved bookmarks with an index number");
-            Console.WriteLine("- delete[index]: deletes the specified bookmark");
+            Console.WriteLine("- delete (2 options)");
+            Console.WriteLine("\t- delete [bookmark name]: deletes the specified bookmark using it's name");
+            Console.WriteLine("\t- delete [bookmark index]: deletes the specified bookmark using it's index (you can look up the index using the list command)");
             Console.WriteLine("- deleteall: deletes all saved bookmarks");
             Console.WriteLine("- help: shows the help text");
         }
